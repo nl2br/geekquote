b24097169c79459fe526a97cfb93e46d2842e45e
serviceinformatique <*serVedu35>
2019-03-12 11:30:01 +0100

    Add adapter and set the adapter to the view e

--- app/src/main/java/fr/mds/geekquote/activity/QuoteListActivity.java
+++ app/src/main/java/fr/mds/geekquote/activity/QuoteListActivity.java
@@ -27,6 +27,7 @@ public class QuoteListActivity extends Activity {
     private ListView lv_quote_list_list;
     private TextView tv_quote_list_list;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
